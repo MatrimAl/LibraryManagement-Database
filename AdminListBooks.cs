89bab3a75f6898508f066c2fd43f3a8e4b3a57e7
IsmailOzsy <ismailozsoy969@gmail.com>
2023-03-24 03:28:11 +0300

    edit

--- AdminListBooks.cs
+++ AdminListBooks.cs
@@ -10,6 +10,9 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Drawing.Image;
+
+
 
 namespace LibraryManagement
 {
@@ -34,69 +37,91 @@ namespace LibraryManagement
             dataGridView1.DataSource = dt;  
 
         }
-        public void Insert(String bookId, String bookName, String authorName,String bookStock)
+        public void insertDatabase(String bookId, String bookName, String authorName,String bookStock,byte[] image)
         {
             conn.Open();
-            String querry = "UPDATE LibraryBooks SET BookID = @bookid , BookName =  @bookname, BookAuthor = @bookauthor, BookStock = @bookstock Where BookID = @bookid , BookName =  @bookname, BookAuthor = @bookauthor, BookStock = @bookstock ";
+            String querry = "INSERT INTO LibraryBooks (BookID, BookName, BookAuthor, BookStock, BookImage) Values (@bookid, @bookname, @bookauthor, @bookstock, @bookimage)";
             SqlCommand cmd = new SqlCommand(querry, conn);
             cmd.Parameters.AddWithValue("@bookid", bookId);
             cmd.Parameters.AddWithValue("@bookname", bookName);
             cmd.Parameters.AddWithValue("@bookauthor", authorName);
             cmd.Parameters.AddWithValue("@bookstock", bookStock);
+            cmd.Parameters.AddWithValue("@bookimage", image);
+
             cmd.ExecuteNonQuery();
             conn.Close();
         }
 
-        public void InsertImage(byte[] image, String BookID)
+        public void updateDatabase(String bookId, String bookName, String authorName, String bookStock, byte[] image)
         {
             conn.Open();
-            String querry = "UPDATE LibraryBooks SET BookImage = @bookimage Where BookID = @bookid";
+            String querry = "UPDATE LibraryBooks SET BookID = @bookid , BookName =  @bookname, BookAuthor = @bookauthor, BookStock = @bookstock, BookImage = @bookimage Where BookID = @bookid ";
             SqlCommand cmd = new SqlCommand(querry, conn);
-            cmd.Parameters.AddWithValue("@bookid", BookID);
+            cmd.Parameters.AddWithValue("@bookid", bookId);
+            cmd.Parameters.AddWithValue("@bookname", bookName);
+            cmd.Parameters.AddWithValue("@bookauthor", authorName);
+            cmd.Parameters.AddWithValue("@bookstock", bookStock);
             cmd.Parameters.AddWithValue("@bookimage", image);
             cmd.ExecuteNonQuery();
             conn.Close();
         }
         byte[] ConvertImageToBytes(Image img)
         {
-            using (MemoryStream ms = new MemoryStream())
+            if (img == null) return null;
+            else
             {
-                img.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
-                return ms.ToArray();
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    img.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                    return ms.ToArray();
+                }
+
             }
+            
         }
-        public Image ConvertByteArrayToImage(byte[] data)
+        public System.Drawing.Image ConvertByteArrayToImage(byte[] data)
         {
             System.Drawing.ImageConverter converter = new System.Drawing.ImageConverter();
             Image img = (Image)converter.ConvertFrom(data);
             return img;
         }
 
-        
-
-        private void updateButton_Click(object sender, EventArgs e)
+        private void uploadButton_Click(object sender, EventArgs e)
         {
             using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Image files(*.jpg;*.jpeg)|*.jpg;*.jpeg", Multiselect = false })
             {
-                if(ofd.ShowDialog() == DialogResult.OK)
+                if (ofd.ShowDialog() == DialogResult.OK)
                 {
                     pictureBox1.Image = Image.FromFile(ofd.FileName);
-                    //InsertImage(ConvertImageToBytes(pictureBox1.Image));
                 }
             }
         }
-
+        
         private void button1_Click(object sender, EventArgs e)
         {
-            //Insert();
-            LoadData();
-            
+            insertDatabase(idBook.Text, nameBook.Text, authorBook.Text, stockBook.Text, ConvertImageToBytes(pictureBox1.Image));
         }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            updateDatabase(BookID, BookName, BookAuthor, BookStock, ConvertImageToBytes(pictureBox1.Image));
+        }
+
+        private void refreshButton_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
 
         }
+        String BookID;
+        String BookName;
+        String BookAuthor;
+        String BookStock;
+        String BookImage;
 
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
@@ -104,8 +129,6 @@ namespace LibraryManagement
             String BookName = dataGridView1.CurrentRow.Cells[1].Value.ToString();
             String BookAuthor = dataGridView1.CurrentRow.Cells[2].Value.ToString();
             String BookStock = dataGridView1.CurrentRow.Cells[3].Value.ToString();
-            String BookImage = dataGridView1.CurrentRow.Cells[4].Value.ToString();
-            Insert(BookID, BookName, BookAuthor, BookStock);
             foreach (DataGridViewRow row in dataGridView1.Rows)
             {
                 if (dataGridView1.CurrentCell.ColumnIndex.Equals(4) && e.RowIndex != -1)
@@ -115,7 +138,7 @@ namespace LibraryManagement
                         if (ofd.ShowDialog() == DialogResult.OK)
                         {
                             pictureBox1.Image = Image.FromFile(ofd.FileName);
-                            InsertImage(ConvertImageToBytes(pictureBox1.Image), BookID);
+                          //  InsertImage(ConvertImageToBytes(pictureBox1.Image), BookID);
                         }
                     }
                 }
@@ -124,6 +147,6 @@ namespace LibraryManagement
                 
         }
 
-        
+       
     }
 }
